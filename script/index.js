2f52e6bb4d96dcae0296d7a098811f3f1b5e4bda
Marianna <marianna.albrici@gmail.com>
2023-03-21 18:18:29 +0100

    Started logic

---
+++ script/index.js
@@ -0,0 +1,50 @@
+let randomArtist = Math.floor(Math.random() * 1000) + 1;
+let randomAlbum = Math.floor(Math.random() * 100000) + 10000000;
+const endpointArtist = 'https://striveschool-api.herokuapp.com/api/deezer/artist/' + randomArtist;
+const endpointAlbum = 'https://striveschool-api.herokuapp.com/api/deezer/album/' + randomAlbum;
+const trackList = 'https://striveschool-api.herokuapp.com/api/deezer/artist/' + randomArtist + '/top?limit=50';
+const URLParams = new URLSearchParams(window.location.search);
+const selectedId = URLParams.get('id');
+
+//Population of the fields
+
+window.onload = () => {
+	// try {
+	// 	fetch(endpointAlbum)
+	// 		.then((responseObj) => responseObj.json())
+	// 		.then((album) => {
+	// 			let albumName = document.getElementById('albumNameTop');
+	// 			albumName.textContent = album.title;
+	// 			let albumArtist = document.getElementById('albumArtistTop');
+	// 			albumArtist.textContent = album.artist.name;
+	// 			let albumCover = document.getElementById('albumCoverTop');
+	// 			albumCover.src = album.cover_big;
+	// 			console.log(endpointAlbum);
+	// 		});
+	// } catch (error) {
+	// 	console.log('ERROR' + error);
+	// }
+
+	try {
+		fetch(trackList)
+			.then((responseObj) => responseObj.json())
+			.then((trackList) => {
+				console.log(trackList);
+				trackList.data.forEach((track) => {
+					let col = document.querySelectorAll('.madeForYou');
+					col.innerHTML += `<button class="btn">
+                                            <div class="card p-2 bg-secondary">
+                                            <img src="${track.album.cover}" class="card-img-top"
+                                                alt="${track.title}">
+                                            <div class="card-body text-start">
+                                                <p class="card-text fs-10 text-light">${track.title}</p>
+                                                <p class="card-text fs-10 fw-light text-info">${track.artist.name}</p>
+                                            </div>
+                                            </div>
+                                        </button>`;
+				});
+			});
+	} catch (error) {
+		console.log('ERROR' + error);
+	}
+};

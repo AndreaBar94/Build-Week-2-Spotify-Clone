4d9b63b716876b0d392411736f474c5a7788fc99
AndreaBar94 <122469003+AndreaBar94@users.noreply.github.com>
2023-03-21 17:59:46 +0100

    dom population done

---
+++ script/artist.js
@@ -0,0 +1,53 @@
+//id generator for casual artists
+let casualId = Math.floor(Math.random() * 1000) + 1;
+const endpoint = "https://striveschool-api.herokuapp.com/api/deezer/artist/" + casualId;
+const trackList = "https://striveschool-api.herokuapp.com/api/deezer/artist/" + casualId + "/top?limit=50";
+const URLParams = new URLSearchParams(window.location.search);
+const selectedId = URLParams.get("id");
+
+//fill the html with artist info
+window.onload = () => {
+	try {
+		fetch(endpoint)
+			.then((responseObj) => responseObj.json())
+			.then((artist) => {
+				let artistName = document.getElementById("artistName");
+				let followers = document.getElementById("followers");
+				let artistImg = document.getElementById("artistThumbnail");
+				let monthlyListener = document.getElementById("monthlyListener");
+				artistName.textContent = artist.name;
+				followers.textContent = artist.nb_fan + " followers";
+				monthlyListener.textContent = artist.nb_fan + " ascoltatori mensili";
+				artistImg.style.backgroundImage = `url(${artist.picture_xl})`;
+			});
+
+		fetch(trackList)
+			.then((responseList) => responseList.json())
+			.then((trackList) => {
+				console.log(trackList);
+				let popularList = document.getElementById("popularList");
+				popularList.innerHTML = "";
+				trackList.data.forEach((track) => {
+					let liElement = document.createElement("li");
+					liElement.classList.add("py-3");
+					liElement.innerHTML = `
+                        <div class="row row-cols-3">
+                            <div class="col-6 fs-10">
+                                <img src="${track.album.cover_small}" alt="" width="35px" />
+                                <span>${track.title}</span>
+                            </div>
+                            <div class="col-3">
+                                <span>Rank ${track.rank}</span>
+                            </div>
+                            <div class="col-3">
+                                <span>${track.duration}</span>
+                            </div>
+                        </div>
+                    `;
+					popularList.appendChild(liElement);
+				});
+			});
+	} catch (error) {
+		console.log("errore" + error);
+	}
+};

605919429e15b2268c4a524201835d81939c198a
luqi2000 <luqmanmohammad09@gmail.com>
2023-03-22 21:05:54 +0000

    agg

--- script/album.js
+++ script/album.js
@@ -7,41 +7,42 @@ const endpoint = " https://striveschool-api.herokuapp.com/api/deezer/album/" + s
 const tracklist = " https://striveschool-api.herokuapp.com/api/deezer/album/" + selectedId;
 
 window.onload = () => {
-	try {
-		fetch(endpoint)
-			.then((responseObj) => responseObj.json())
-			.then((album) => {
-				console.log(album);
-				let imgAlbum = document.querySelector("#imgAlbum");
-				let titleAlbum = document.querySelector("#titleAlbum");
-				let NumTrack = document.getElementById("NumTrack");
-				let tipe = document.getElementById("tipe");
-				imgAlbum.src = `${album.cover_medium}`;
-				titleAlbum.textContent = album.title;
-				NumTrack.textContent = album.nb_tracks + " brani";
-				tipe.textContent = album.record_type;
-			});
+  try {
+    fetch(endpoint)
+      .then(responseObj => responseObj.json())
+      .then(album => {
+        console.log(album);
+        let imgAlbum = document.querySelector("#imgAlbum");
+        let titleAlbum = document.querySelector("#titleAlbum");
+        let NumTrack = document.getElementById("NumTrack");
+        let tipe = document.getElementById("tipe");
+        imgAlbum.src = `${album.cover_medium}`;
+        titleAlbum.textContent = album.title;
+        NumTrack.textContent = album.nb_tracks + " brani";
+        tipe.textContent = album.record_type;
+      });
 
-		fetch(tracklist)
-			.then((responseList) => responseList.json())
-			.then((trackslist) => {
-				console.log(trackslist);
+    fetch(tracklist)
+      .then(responseList => responseList.json())
+      .then(trackslist => {
+        console.log(trackslist);
 
-				let containerAlbum = document.getElementById("containerAlbum");
-				const olAlbum = document.createElement("ol");
-				containerAlbum.appendChild(olAlbum);
-				trackslist.tracks.data.forEach((track, index) => {
-					const liAlbum = document.createElement("li");
-					liAlbum.classList.add("mt-2");
-					let duration = (track.duration / 60).toFixed(2) + " minuti";
-					liAlbum.innerHTML += `
+        let containerAlbum = document.getElementById("containerAlbum");
+        const olAlbum = document.createElement("ol");
+        olAlbum.setAttribute("id", "popularList");
+        containerAlbum.appendChild(olAlbum);
+        trackslist.tracks.data.forEach((track, index) => {
+          const liAlbum = document.createElement("li");
+          liAlbum.classList.add("py-3");
+          let duration = (track.duration / 60).toFixed(2) + " minuti";
+          liAlbum.innerHTML += `
             <div id="AlbumList" class="row align-items-start">
               <div id="album-title" class="col12 col-md-5 mb-3">
-                <a href="#" class="card-title mb-2" style="color: white">
+                <a href="#" class="card-title mb-2 text-decoration-none text-light">
                   ${track.title}
                 </a>
                 <br>
-                <a href="artist.html?id= ${track.artist.Id}" class="album-artist text-info">${track.artist.name}</a>
+                <a href="artist.html?id= ${track.artist.Id}" class="album-artist text-info text-decoration-none">${track.artist.name}</a>
               </div>
               <div class="col-4 d-none d-md-block text-info">
                 <p class="riproduction">2819873</p>
@@ -51,15 +52,15 @@ window.onload = () => {
               </div>
             </div>
           `;
-					olAlbum.appendChild(liAlbum);
-				});
-			});
-	} catch (error) {
-		console.log("errore" + error);
-	}
+          olAlbum.appendChild(liAlbum);
+        });
+      });
+  } catch (error) {
+    console.log("errore" + error);
+  }
 };
 
 let previousBtn = document.querySelector(".previousBtn");
 previousBtn.addEventListener("click", function () {
-	window.location.href = "index.html";
+  window.location.href = "index.html";
 });

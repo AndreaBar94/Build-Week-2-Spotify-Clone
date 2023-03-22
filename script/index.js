92c36ec6a759f49eaa186b11b9514bc2aea640b2
Marianna <marianna.albrici@gmail.com>
2023-03-22 15:50:58 +0100

    Day 3 - JS and graphics

--- script/index.js
+++ script/index.js
@@ -18,7 +18,7 @@ window.onload = () => {
 				let albumName = document.getElementById('albumNameTop');
 				albumName.textContent = album.title;
 				let albumArtist = document.getElementById('albumArtistTop');
-				albumArtist.innerHTML = `<a href="artist.html?id=${album.artist.id}"> ${album.artist.name} </a>`;
+				albumArtist.innerHTML = `<a href="artist.html?id=${album.artist.id}" class="text-decoration-none link-light"> ${album.artist.name} </a>`;
 				let albumCover = document.getElementById('albumCoverTop');
 				albumCover.src = album.cover_big;
 				console.log(endpointAlbum);
@@ -29,7 +29,7 @@ window.onload = () => {
 
 	//Made for you population
 	try {
-		fetch(endpointTrackList)
+		fetch('https://striveschool-api.herokuapp.com/api/deezer/artist/1/top?limit=50')
 			.then((responseObj) => responseObj.json())
 			.then((trackList) => {
 				console.log(trackList);
@@ -40,7 +40,7 @@ window.onload = () => {
 							<img src="${trackList.data[index].album.cover_medium}" class="card-img-top" alt="${trackList.data[index].title}">
 							<div class="card-body text-start">
 								<p class="card-text fs-10 text-light">${trackList.data[index].title_short}</p>
-								<p class="card-text fs-10 fw-light text-info"><a href="artist.html?id=${trackList.data[index].artist.id}" class="text-decoration-none"> ${trackList.data[index].artist.name} </a></p>
+								<p class="card-text fs-10 fw-light text-info"><a href="artist.html?id=${trackList.data[index].artist.id}" class="text-decoration-none link-info"> ${trackList.data[index].artist.name} </a></p>
 							</div>
 							</div>`;
 				});
@@ -51,7 +51,7 @@ window.onload = () => {
 
 	//Your Episodes population
 	try {
-		fetch(endpointTrackList)
+		fetch('https://striveschool-api.herokuapp.com/api/deezer/artist/2/top?limit=50')
 			.then((responseObj) => responseObj.json())
 			.then((trackList) => {
 				console.log(trackList);
@@ -62,8 +62,77 @@ window.onload = () => {
 							<img src="${trackList.data[index].album.cover_medium}" class="card-img-top" alt="${trackList.data[index].title}">
 							<div class="card-body text-start">
 								<p class="card-text fs-10 text-light">${trackList.data[index].title_short}</p>
-								<p class="card-text fs-10 fw-light text-info"><a href="artist.html?id=${trackList.data[index].artist.id}" class="text-decoration-none"> ${trackList.data[index].artist.name} </a></p>
+								<p class="card-text fs-10 fw-light text-info"><a href="artist.html?id=${trackList.data[index].artist.id}" class="text-decoration-none link-info"> ${trackList.data[index].artist.name} </a></p>
 							</div>
+							</div>`;
+				});
+			});
+	} catch (error) {
+		console.log('ERROR' + error);
+	}
+
+	try {
+		fetch('https://striveschool-api.herokuapp.com/api/deezer/artist/1/top?limit=50')
+			.then((responseObj) => responseObj.json())
+			.then((trackList) => {
+				console.log(trackList);
+				let cols = document.querySelectorAll('.mobileEpisode');
+				cols.forEach((col, index) => {
+					col.innerHTML += `
+									<div class="card border border-0 bg-dark" style="width: 150px">
+									<a href="album.html?id=${trackList.data[index].album.id}" class="text-decoration-none link-info">
+									<img src="${trackList.data[index].album.cover_medium}" class="card-img-top rounded-0"alt="episodeCover">
+									</a>
+									<div class="card-body text-start px-1 mobileHomeMainCards">
+										<p class="card-text text-light fs-10">${trackList.data[index].title_short}</p>
+										<p class="card-text text-info fs-11 fw-light"><a href="artist.html?id=${trackList.data[index].artist.id}" class="text-decoration-none link-info"> ${trackList.data[index].artist.name} </a></p>
+									</div>
+								</div>`;
+				});
+			});
+	} catch (error) {
+		console.log('ERROR' + error);
+	}
+
+	try {
+		fetch('https://striveschool-api.herokuapp.com/api/deezer/artist/2205/top?limit=50')
+			.then((responseObj) => responseObj.json())
+			.then((trackList) => {
+				console.log(trackList);
+				let cols = document.querySelectorAll('.homeMainAlbumCard');
+				cols.forEach((col, index) => {
+					col.innerHTML += `
+								<div class="card border border-0 bg-dark" style="width: 150px">
+									<a href="album.html?id=${trackList.data[index].album.id}" class="text-decoration-none link-info">
+									<img src="${trackList.data[index].album.cover_medium}" class="card-img-top rounded-0"alt="episodeCover">
+									</a>
+									<div class="card-body text-start px-1 mobileHomeMainCards">
+										<p class="card-text text-light fs-10">${trackList.data[index].title_short}</p>
+										<p class="card-text text-info fs-11 fw-light"><a href="artist.html?id=${trackList.data[index].artist.id}" class="text-decoration-none link-info"> ${trackList.data[index].artist.name} </a></p>
+									</div>
+								</div>`;
+				});
+			});
+	} catch (error) {
+		console.log('ERROR' + error);
+	}
+
+	try {
+		fetch('https://striveschool-api.herokuapp.com/api/deezer/artist/30')
+			.then((responseObj) => responseObj.json())
+			.then((artist) => {
+				console.log(artist);
+				let cols = document.querySelectorAll('.homeMainArtistCard');
+				cols.forEach((col, index) => {
+					col.innerHTML += `
+								<div class="card border border-0 bg-dark" style="width: 150px">
+								<a href="album.html?id=${artist.id}" class="text-decoration-none link-info">
+									<img src="${artist.picture}" class="card-img-top rounded-circle"
+									</a>
+									alt="episodeCover">
+								<div class="card-body text-start px-1 mobileHomeMainCards">
+									<p class="card-text text-light fs-10"><a href="artist.html?id=${artist.id}" class="text-decoration-none link-info"> ${artist.name} </a></p>
+								</div>
 							</div>`;
 				});
 			});

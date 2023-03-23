721e3c204b57c1e6cfbffbc819dc2d2e788d81d7
AndreaBar94 <122469003+AndreaBar94@users.noreply.github.com>
2023-03-23 13:51:30 +0100

    username in session storage

--- script/login.js
+++ script/login.js
@@ -1,8 +1,15 @@
 let loginForm = document.querySelector("form");
 let loginBtn = document.getElementById("loginBtn");
+let userName = document.getElementById("userName");
+let users = [];
 
 loginForm.addEventListener("submit", function (event) {
 	event.preventDefault();
+	if (sessionStorage.getItem("Username")) {
+		users = JSON.parse(sessionStorage.getItem("Username"));
+	}
+	users.push(userName.value);
+	sessionStorage.setItem("Username", JSON.stringify(users));
 	window.location.href = "index.html";
 });
 

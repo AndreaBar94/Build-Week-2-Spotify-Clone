c47fee208a140344d5a8fe4535a70faf14ddf684
AndreaBar94 <122469003+AndreaBar94@users.noreply.github.com>
2023-03-24 09:09:48 +0100

    search terms with song and album

--- script/search.js
+++ script/search.js
@@ -1,17 +1,29 @@
-let searchInput = document.getElementById('searchInput');
+let searchInput = document.getElementById("searchInput");
 
 let searched = () => {
 	try {
-		searchInput.addEventListener('keydown', function (event) {
+		searchInput.addEventListener("keydown", function (event) {
 			if (event.keyCode === 13) {
 				// Invio
 				event.preventDefault(); // previene il comportamento predefinito della pressione del tasto Invio
-				fetch('https://striveschool-api.herokuapp.com/api/deezer/search?q=' + searchInput.value)
+				fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=" + searchInput.value)
 					.then((responseObj) => responseObj.json())
 					.then((artist) => {
 						let artistId = artist.data[0].artist.id; // prende il primo artista restituito (se presente)
 						if (artist) {
-							window.location.href = 'artist.html?id=' + artistId; // reindirizza alla pagina dell'artista
+							window.location.href = "artist.html?id=" + artistId; // reindirizza alla pagina dell'artista
+						}
+					})
+					.then((song) => {
+						let songId = song.data[0].title;
+						if (song) {
+							window.location.href = "album.html?id=" + songId;
+						}
+					})
+					.then((album) => {
+						let albumId = album.id;
+						if (song) {
+							window.location.href = "album.html?id=" + albumId;
 						}
 					})
 					.catch((error) => {
